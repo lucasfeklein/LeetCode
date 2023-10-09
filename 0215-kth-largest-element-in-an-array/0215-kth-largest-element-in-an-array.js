c7f88a79ab808ae1e20afe30793f396aa5350a50
Klein <116522160+lucasfeklein@users.noreply.github.com>
2023-10-09 10:52:47 +0700

    Time: 145 ms (58.30%), Space: 52.5 MB (44.90%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    const sortedArr = nums.sort((a, b) => a - b)
+
+    return sortedArr[sortedArr.length - k]
+};
